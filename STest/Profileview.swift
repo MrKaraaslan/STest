dfa583fa02df39f0a1e123bc4441d623b58d489d
Mr. Karaaslan <developerkaraaslan@gmail.com>
2020-04-09 20:10:26 +0300

    edited profile view to use image picker

--- STest/Profileview.swift
+++ STest/Profileview.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Profileview: View {
     
     @State var image: Image? = nil
+    @State var showImageSheet = false
     @State var showImagePicker = false
     @State var useCamera = false
     
@@ -18,27 +19,68 @@ struct Profileview: View {
         VStack {
             
             ZStack(alignment: .bottomTrailing) {
-                ZStack {
-                    Color.gray
+                if(image != nil){
+                    image?.resizable()
+                        .aspectRatio(contentMode: .fill)
                         .frame(width: 150, height: 150)
                         .clipShape(Circle())
-                    
-                    Image(systemName: "camera").imageScale(.large)
                 }
+                else {
+                    ZStack {
+                        Color.gray
+                            .frame(width: 150, height: 150)
+                                            
+                        Image(systemName: "camera").imageScale(.large)
+
+                    }.clipShape(Circle())
+                }
+                
                 Button(action: {
-                    self.showImagePicker = true
+                    self.showImageSheet = true
                 }) {
                     Image(systemName: "person.crop.circle.badge.plus")
-                    .imageScale(.large)
-                    .foregroundColor(.mainColor)
-                    .frame(width: 50, height: 50)
-                    .background(Color.white)
-                    .clipShape(Circle())
+                        .imageScale(.large)
+                        .foregroundColor(.mainColor)
+                        .frame(width: 50, height: 50)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .actionSheet(isPresented: $showImageSheet){
+                            ActionSheet(
+                                title: Text("title"),
+                                buttons: image == nil ? [
+                                    .default(Text("Fotoğraf Çek"), action: {
+                                        self.useCamera = true
+                                        self.showImagePicker = true
+                                    }),
+                                    .default(Text("Galeriden Seç"), action: {
+                                        self.useCamera = false
+                                        self.showImagePicker = true
+                                    }),
+                                    .cancel(Text("Vazgeç"))
+                                ]:[
+                                    .default(Text("Fotoğraf Çek"), action: {
+                                        self.useCamera = true
+                                        self.showImagePicker = true
+                                    }),
+                                    .default(Text("Galeriden Seç"), action: {
+                                        self.useCamera = false
+                                        self.showImagePicker = true
+                                    }),
+                                    .destructive(Text("Fotoğrafı Kaldır"), action: {
+                                        self.deleteImage()
+                                    }),
+                                    .cancel(Text("Vazgeç"))
+                            ])
+                    }
                 }.sheet(isPresented: $showImagePicker, content: {
                     CaptureImageView(isShown: self.$showImagePicker, image: self.$image, useCamera: self.$useCamera)
                 })
             }
         }
+    }
+    
+    func deleteImage(){
+        self.image = nil //: firebase
     }
 }
 
