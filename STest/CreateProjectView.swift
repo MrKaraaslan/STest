ff289ddbe2cddc0eedbb5edd1a78ac2f4761a03c
Mr. Karaaslan <developerkaraaslan@gmail.com>
2020-04-17 23:06:38 +0300

    added CreateProjectView and made some changes

---
+++ STest/CreateProjectView.swift
@@ -0,0 +1,21 @@
+//
+//  CreateProjectView.swift
+//  STest
+//
+//  Created by Mehmet Karaaslan on 17.04.2020.
+//  Copyright © 2020 Mehmet Karaaslan. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateProjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateProjectView()
+    }
+}
