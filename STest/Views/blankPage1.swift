9f71bdf7064bb08e285ed3870e5dc86c6b076990
Mr. Karaaslan <developerkaraaslan@gmail.com>
2020-06-18 17:39:41 +0300

    added login and logout actions

---
+++ STest/Views/blankPage1.swift
@@ -0,0 +1,21 @@
+//
+//  blankPage1.swift
+//  STest
+//
+//  Created by Mehmet Karaaslan on 18.06.2020.
+//  Copyright © 2020 Mehmet Karaaslan. All rights reserved.
+//
+
+import SwiftUI
+
+struct blankPage1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct blankPage1_Previews: PreviewProvider {
+    static var previews: some View {
+        blankPage1()
+    }
+}
---
+++ STest/Views/blankPage2.swift
@@ -0,0 +1,21 @@
+//
+//  blankPage.swift
+//  STest
+//
+//  Created by Mehmet Karaaslan on 18.06.2020.
+//  Copyright © 2020 Mehmet Karaaslan. All rights reserved.
+//
+
+import SwiftUI
+
+struct blankPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct blankPage_Previews: PreviewProvider {
+    static var previews: some View {
+        blankPage()
+    }
+}
